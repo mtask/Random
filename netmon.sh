9f1958a1ca43c716bfd0c3734976233fa01353fb
mtask <taskinen.miikka@gmail.com>
2015-10-12 17:01:07 +0000

    netmon

---
+++ netmon.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+
+#Log internet connection status while script is running.
+#Log  is written in home directory of the user
+#Usage:
+# #chmod 755 netmon.sh
+# $./netmon.sh
+#or
+# $bash netmon.sh
+#
+# Check gateway when internet connection down.
+# $./netmon.sh -g
+
+file="netmon_"$(date +%D.log | tr "/" "_")
+
+help_menu(){
+clear
+echo "Example usage: ./net_test.sh"
+echo "-g check connection to gateway if internet connection down"
+exit 0
+}
+
+
+gateway_state(){
+gw=$(ip route | awk '/default/ { print $3 }')
+
+if [ $(ping -c 1 $gw | grep icmp* | wc -l) = 0 ];
+    then
+        while [ $(ping -c1 $gw | grep icmp* | wc -l) = 0 ]
+        do
+            echo "No connection to gateway"
+            sleep 1
+            clear
+        done
+fi
+}
+
+
+if [ $# = 1 ];
+then
+    if [ $1 =  "-h" ];
+    then
+        help_menu
+    elif [ $1 = "-g" ];
+    then
+        gw=$2
+    fi
+fi
+
+echo $(date +%H-%M-%S): "[!] Test started" >> $file
+while true;
+do
+    if [ $(ping -c 1 google.com | grep icmp* | wc -l) = 1 ]
+    then
+        clear
+        echo "Internet connection is alive"
+        sleep 1
+        clear
+    else
+        echo -e "\n"$(date +%H.%M.%S)": Internet connection down" >> $file
+        if [ $(ping -c 1 8.8.8.8 | grep icmp* | wc -l) = 1 ]
+        then
+            echo -e "\n"$(date +%H.%M.%S)": Issue with DNS-server" >> $file   
+        else
+            if [ $# -gt 0 ] && [ $1 = "-g" ]
+            then
+               gateway_state
+            fi
+        fi
+        
+        while [ $(ping -c 1 8.8.8.8 | grep icmp* | wc -l) != 1 ]
+        do
+            clear
+            echo "Internet connection dead"
+            sleep 1
+            clear
+        done
+        echo $(date +%H.%M.%S)": Connection back alive" >> $file
+    fi
+done 
+
+
