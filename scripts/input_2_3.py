bfa788f69eb8a295146d858fdcbfaed96150cf51
mtask <taskinen.miikka@gmail.com>
2015-10-23 17:00:26 +0300

    2_3

--- scripts/input_2_3.py
+++ scripts/input_2_3.py
@@ -1,8 +1,8 @@
 def get_input(prompt):
     """
-   Function Get input from the user maintaining the python compatibility 
-with earlier and newer versions.
-   """
+    Function Get input from the user maintaining the python compatibility 
+    with earlier and newer versions.
+    """
     if sys.hexversion > 0x03000000:
         return input(prompt)
     else:
