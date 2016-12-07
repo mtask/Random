68aff227ebd21c11d1806eab75d02785b829b8c2
Miikka <taskinen.miikka@gmail.com>
2016-12-07 12:10:01 +0200

    Update netdrivemon.py

--- python/netdrivemon.py
+++ python/netdrivemon.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import datetime
+import sys
 
 ########################################
 # Check if path in mapped drive exists #
@@ -12,24 +13,37 @@ logging.basicConfig(filename = 'webdavcheck.log', level = logging.DEBUG)
 logging.info('Started: ' + str(datetime.datetime.now()))
 
 
+if len(sys.argv) > 4:
+    server = sys.argv[1]
+    driveletter = sys.argv[2]
+    user = sys.argv[3]
+    passw = sys.argv[4]
+    print "Server is "+server
+    print "Drive letter is "+driveletter
+    logging.info("Server is "+server)
+    logging.info("Drive letter is "+driveletter)
+else:
+    logging.warning('No arguments given.')
+    
+
 try:
     #####
-    # Put path here which is accessible IF networkd drive is mapped and connected
+    #Try directory listing from drive
     #####
-    os.listdir("W:/mapped/webdav/path")
+    os.listdir(driveletter+":/")
     logging.info('Path is accessible')
 except OSError as oe:
     # If path is not accessible'
     logging.warning('Path cannot be accessed')
     logging.info('Trying to re-map: ')
     ###
-    # Put correct drive letter here...
+    # Delete mapping from drive letter and..
     ###
-    os.system("NET USE W: /DELETE /Y")
+    os.system("NET USE "+driveletter+": /DELETE /Y")
     ###
-    # ..and here + server info
+    # ..map drive
     ###
-    res = os.system('NET USE W: "https://server.com" /User:username pass')
+    res = os.system('NET USE '+driveletter+': "'+server+'" /User:'+user+' '+passw)
     if res == 0:
        logging.info('Drive remapped successfully')
     else :
