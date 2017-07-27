7e53c0a654aa08aef8e48787aad5a493fc3df79d
Miikka <taskinen.miikka@gmail.com>
2017-07-27 13:31:49 +0300

    Update setupdebiansrv.sh

--- bash/setupdebiansrv.sh
+++ bash/setupdebiansrv.sh
@@ -4,6 +4,7 @@ apt-get update -y && apt-get upgrade -y && apt-get install -y ufw sudo && clear
 
 read -p "New User: " USERNAME
 useradd -m -G users,sudo -s /bin/bash $USERNAME  && clear
+chpasswd $USERNAME
 
 read -p "SSH port: " SSH_PORT
 sed -i -e 's/Port 22/Port '"$SSH_PORT"'/g' /etc/ssh/sshd_config
